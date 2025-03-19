b82fbd0ce6ce283ecdc310edeef198ab18ca41ae
tbattur22 <tbattur22@yahoo.com>
2025-03-19 12:09:19 -0500

    Changed base path in vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/tbattur22/demo-product-list",
+  base: "/demo-product-list",
   plugins: [react()],
 })
